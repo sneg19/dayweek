e52999b34299b03eb40a276cd1d8af86a5fad88b
sneg <sneg.sbg@gmail.com>
2023-01-20 00:02:20 +0300

    not program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.Write("Введите число, обозначающее день недели: ");
+int day_number = int.Parse(Console.ReadLine()!);
+
+if (day_number < 8)
+{
+    if (day_number = 7)
+     {
+        Console.WriteLine($"{day_number} - выходной день");
+     }
+     else
+        {
+            if (day_number = 6)
+                {
+                    Console.WriteLine($"{day_number} - выходной день");
+                }
+            else
+                {
+                    Console.WriteLine($"{day_number} - рабочий день");
+                }
+        }
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует!");
+}
